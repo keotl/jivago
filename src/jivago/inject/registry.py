a8f53458c6ee6c8a12d01a5a1afa1ee3da0e8aec
Kento A. Lauzon <kento.lauzon@ligature.ca>
2018-03-12 19:27:02 -0400

    generic registering annotation class. TODO separate registries

--- src/jivago/inject/registry.py
+++ src/jivago/inject/registry.py
@@ -1,9 +1,36 @@
+from typing import Callable
+
+
 class Registry(object):
-    components = set()
+    content = {}
 
 
-def Component(wrapped_class: type) -> type:
-    # Registry.components[wrapped_class.__module__] = wrapped_class
-    Registry.components.add(wrapped_class)
+class Annotation(object):
+
+    def __init__(self, decorator: Callable):
+        self.decorator = decorator
+        self.registry = Registry()
+
+    def __call__(self, target):
+        self.registry.content[self.decorator] = target
+        return self.decorator(target)
+
+
+@Annotation
+def Singleton(wrapped_class: type) -> type:
     return wrapped_class
 
+
+@Annotation
+def Component(wrapped_class: type) -> type:
+    return wrapped_class
+
+
+# TODO remove testing dummy classes
+@Singleton
+@Component
+class MultiAnnotated(object):
+    pass
+
+
+print(Registry.content)
