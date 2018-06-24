382f8b831ade3dd321fe433cbeafacc48c3c9d08
Kento A. Lauzon <kento.lauzon@ligature.ca>
2018-06-24 15:46:55 -0400

    HTTP 206 partial content handler

--- test_utils/request_builder.py
+++ test_utils/request_builder.py
@@ -14,5 +14,9 @@ class RequestBuilder(object):
         self._body = body
         return self
 
+    def headers(self, headers: dict) -> "RequestBuilder":
+        self._headers = headers
+        return self
+
     def build(self):
         return Request(self._method, self._path, self._headers, self._queryString, self._body)
