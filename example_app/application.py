5ee1cff03d59577b0c273ecb3bfe38977a87edeb
Kento A. Lauzon <kento.lauzon@ligature.ca>
2018-11-18 13:17:35 -0500

    basic logging + banner

--- example_app/application.py
+++ example_app/application.py
@@ -1,6 +1,9 @@
+import logging
+
 import example_app
 from jivago.jivago_application import JivagoApplication
 
 if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
     app = JivagoApplication(example_app, debug=True)
     app.run_dev()
