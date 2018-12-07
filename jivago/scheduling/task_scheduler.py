090d0fd0b83b2956b31173e56e4656d37df481ca
Kento A. Lauzon <kento.lauzon@ligature.ca>
2018-12-07 09:06:47 -0500

    task_scheduler thread lock fix

--- jivago/scheduling/task_scheduler.py
+++ jivago/scheduling/task_scheduler.py
@@ -14,13 +14,11 @@ class TaskScheduler(object):
         self.lock = Lock()
 
     def schedule_task(self, runnable_class: type, schedule: Schedule):
-        self.lock.acquire()
-        task_runner = ScheduledTaskRunner(runnable_class, schedule, self.service_locator)
-        self.task_runners.append(task_runner)
-        task_runner.start()
-        self.lock.release()
+        with self.lock:
+            task_runner = ScheduledTaskRunner(runnable_class, schedule, self.service_locator)
+            self.task_runners.append(task_runner)
+            task_runner.start()
 
     def stop(self):
-        self.lock.acquire()
-        Stream(self.task_runners).forEach(lambda x: x.stop())
-        self.lock.release()
+        with self.lock:
+            Stream(self.task_runners).forEach(lambda x: x.stop())
