3af7f997c8f88935591dc393ad8f712e85dd589a
Kento A. Lauzon <kento.lauzon@ligature.ca>
2018-12-26 14:58:34 -0500

    cors preflight handling based on origin

--- jivago/wsgi/routing/cors/cors_preflight_resource.py
+++ jivago/wsgi/routing/cors/cors_preflight_resource.py
@@ -1,11 +1,29 @@
 from jivago.wsgi.request.headers import Headers
+from jivago.wsgi.request.response import Response
+
+ALLOW_ORIGIN_HEADER = 'Access-Control-Allow-Origin'
 
 
 class CorsPreflightResource(object):
 
     def __init__(self, cors_headers: Headers):
         self.cors_headers = cors_headers
+        self.allowed_origin = self.cors_headers[
+            ALLOW_ORIGIN_HEADER] if ALLOW_ORIGIN_HEADER in self.cors_headers else '*'
 
-    def preflight(self, headers: Headers) -> Headers:
-        # TODO implement
-        return self.cors_headers
+    def preflight(self, request: Headers) -> Response:
+        origin = request['Origin']
+        if self.should_allow(origin):
+            response = Response(200, self.cors_headers, "")
+        else:
+            response = Response(400, self.cors_headers, "")
+
+        if 'Vary' not in response.headers and response.headers[ALLOW_ORIGIN_HEADER] != '*':
+            response.headers['Vary'] = 'Origin'
+
+        return response
+
+    def should_allow(self, origin: str) -> bool:
+        if self.allowed_origin == '*':
+            return True
+        return origin.startswith(self.allowed_origin)
