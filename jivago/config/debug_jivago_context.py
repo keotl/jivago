f3d98dc5d5691dcfa889db313792e484d4f6d889
Kento A. Lauzon <kento.lauzon@ligature.ca>
2019-01-08 15:34:49 -0500

    debug context all allowed cors rule

--- jivago/config/debug_jivago_context.py
+++ jivago/config/debug_jivago_context.py
@@ -1,17 +1,22 @@
 from typing import List, Type
 
 from jivago.config.production_jivago_context import ProductionJivagoContext
+from jivago.config.router_builder import RouterBuilder
 from jivago.lang.annotations import Override
 from jivago.lang.registry import Registry
 from jivago.wsgi.filter.filter import Filter
 from jivago.wsgi.filter.system_filters.error_handling.debug_exception_filter import DebugExceptionFilter
 from jivago.wsgi.filter.system_filters.error_handling.unknown_exception_filter import UnknownExceptionFilter
+from jivago.wsgi.routing.cors.cors_rule import CorsRule
 
 
 class DebugJivagoContext(ProductionJivagoContext):
 
     def __init__(self, root_package: "Module", registry: Registry, banner: bool = True):
         super().__init__(root_package, registry, banner=banner)
+
+    def create_router_config(self) -> RouterBuilder:
+        return super().create_router_config().add_rule(CorsRule("/", {"Access-Control-Allow-Origin": '*'}))
 
     @Override
     def get_default_filters(self) -> List[Type[Filter]]:
