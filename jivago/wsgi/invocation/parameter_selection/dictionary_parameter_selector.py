61e23ca15650030b07695b63bf987146edad6870
Kento A. Lauzon <kento.lauzon@ligature.ca>
2019-01-12 12:51:22 -0500

    fix error with dictionary body parameter when invoking resource with empty body

--- jivago/wsgi/invocation/parameter_selection/dictionary_parameter_selector.py
+++ jivago/wsgi/invocation/parameter_selection/dictionary_parameter_selector.py
@@ -1,4 +1,5 @@
 from jivago.lang.annotations import Override
+from jivago.wsgi.invocation.missing_route_invocation_argument import MissingRouteInvocationArgument
 from jivago.wsgi.invocation.parameter_selection.parameter_selector import ParameterSelector
 from jivago.wsgi.invocation.parameters import RequestBody
 from jivago.wsgi.request.request import Request
@@ -12,4 +13,6 @@ class DictionaryParameterSelector(ParameterSelector):
 
     @Override
     def format_parameter(self, parameter_name: str, parameter_declaration: type, request: Request) -> object:
-        return request.body
+        if isinstance(request.body, dict):
+            return request.body
+        raise MissingRouteInvocationArgument(parameter_name)
