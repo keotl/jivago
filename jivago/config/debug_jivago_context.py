c6a4daff48129fbb1b6e46c7aeb8449aba7f4287
Kento A. Lauzon <kento.lauzon@ligature.ca>
2019-01-13 11:07:06 -0500

    looser debug cors rule

--- jivago/config/debug_jivago_context.py
+++ jivago/config/debug_jivago_context.py
@@ -1,13 +1,13 @@
 from typing import List, Type
 
 from jivago.config.production_jivago_context import ProductionJivagoContext
+from jivago.config.router.cors_rule import CorsRule
 from jivago.config.router.router_builder import RouterBuilder
 from jivago.lang.annotations import Override
 from jivago.lang.registry import Registry
 from jivago.wsgi.filter.filter import Filter
 from jivago.wsgi.filter.system_filters.error_handling.debug_exception_filter import DebugExceptionFilter
 from jivago.wsgi.filter.system_filters.error_handling.unknown_exception_filter import UnknownExceptionFilter
-from jivago.config.router.cors_rule import CorsRule
 
 
 class DebugJivagoContext(ProductionJivagoContext):
@@ -16,7 +16,7 @@ class DebugJivagoContext(ProductionJivagoContext):
         super().__init__(root_package, registry, banner=banner)
 
     def create_router_config(self) -> RouterBuilder:
-        return super().create_router_config().add_rule(CorsRule("/", {"Access-Control-Allow-Origin": '*'}))
+        return super().create_router_config().add_rule(CorsRule("/", {"Access-Control-Allow-Origin": '*', 'Access-Control-Allow-Headers': '*'}))
 
     @Override
     def get_default_filters(self) -> List[Type[Filter]]:
