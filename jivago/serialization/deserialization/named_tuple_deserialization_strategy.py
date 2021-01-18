68e313814c730accc60371792f457b15eb77978a
Kento A. Lauzon <kento.lauzon@ligature.ca>
2021-01-18 14:18:16 -0500

    backwards compatibility for namedtuple deserialization on 3.6

--- jivago/serialization/deserialization/named_tuple_deserialization_strategy.py
+++ jivago/serialization/deserialization/named_tuple_deserialization_strategy.py
@@ -1,3 +1,4 @@
+import sys
 from typing import NamedTuple, Type
 
 from jivago.lang.annotations import Override
@@ -12,15 +13,26 @@ class NamedTupleDeserializationStrategy(DeserializationStrategy):
 
     @Override
     def can_handle_deserialization(self, declared_type: type) -> bool:
-        return hasattr(declared_type, "__bases__") \
-               and tuple in declared_type.__bases__
+        if sys.version_info[0:2] == (3, 6):
+            return hasattr(declared_type, "__bases__") \
+                   and tuple in declared_type.__bases__ \
+                   and hasattr(declared_type, "_field_types")
+
+        return hasattr(declared_type, "__bases__") and tuple in declared_type.__bases__
 
     @Override
     def deserialize(self, obj, declared_type: Type[NamedTuple]) -> NamedTuple:
+        if sys.version_info[0:2] == (3, 6):
+            parameters = {}
+            for name, clazz in declared_type._field_types.items():
+                parameters[name] = self.deserializer.deserialize(obj[name], clazz)
+
+            return declared_type(**parameters)
+
         attributes = declared_type.__annotations__
         parameters = Stream(attributes.items()) \
             .map(lambda name, attribute_type:
-                 (name, self.deserializer.deserialize(obj.get(name), attribute_type)))\
+                 (name, self.deserializer.deserialize(obj.get(name), attribute_type))) \
             .toList()
 
         return declared_type(*parameters)
