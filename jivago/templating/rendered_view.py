c89e6172b6d9b2e94906330a4055f6c880b40790
Kento Lauzon <kento.lauzon@ligature.ca>
2022-07-09 11:19:32 -0400

    Python 3.10 support (#124)
    * python 3.10 pipeline
    
    * bump build dependencies
    
    * CI python 3.10.0
    
    * fix dev dependency version conflict
    
    * setup.py 3.10 python category
    
    * fix optional typing meta detection for python3.10
    
    * downgrade jinja2 for 3.6 build
    
    * RenderedView override content-type

--- jivago/templating/rendered_view.py
+++ jivago/templating/rendered_view.py
@@ -1,5 +1,6 @@
 class RenderedView(object):
 
-    def __init__(self, view_file: str, data):
+    def __init__(self, view_file: str, data,*, content_type: str = "text/html"):
         self.view_file = view_file
         self.data = data
+        self.content_type = content_type
