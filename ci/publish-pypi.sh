3c7a0a95517d2681f13375b31328d20a22c253fb
Kento Lauzon <kento@lauzon.co.uk>
2023-12-24 10:07:44 +0000

    add 3.11 as supported version

--- ci/publish-pypi.sh
+++ ci/publish-pypi.sh
@@ -4,6 +4,7 @@ apk add git
 git describe --tags --exact-match
 if [ $? -ne 0 ]; then
     echo "Not a tagged commit. Skipping."
+    echo "For a package version to be published, HEAD for master should be a tagged commit."
     exit 0;
 fi
 
